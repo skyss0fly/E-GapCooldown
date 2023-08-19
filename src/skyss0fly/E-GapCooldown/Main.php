acf14130b5f8ad19a5d164ae38b533ca9b6467c8
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-19 22:07:21 +1000

    Create Main.php

---
+++ src/skyss0fly/E-GapCooldown/Main.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace skyss0fly\E-GapCooldown;
+
+use pocketmine\{event\Listener, plugin\PluginBase, item\GoldenAppleEnchanted, event\player\PlayerItemUseEvent, event\player\PlayerItemConsumeEvent};
+
+class Main extends PluginBase implements Listener {
+
+public function onLoad():void {
+$this->saveDefaultConfig();
+  
+}
+public function onConsume(PlayerItemConsumeEvent $consumer, GoldenAppleEnchanted $egap) {
+$
+}
+
+
+
+
+
+}
