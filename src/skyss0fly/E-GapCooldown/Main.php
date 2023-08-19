d271cd695be733a125cc0a7a3edd4909787ef777
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-19 22:12:10 +1000

    Update Main.php

--- src/skyss0fly/E-GapCooldown/Main.php
+++ src/skyss0fly/E-GapCooldown/Main.php
@@ -2,20 +2,38 @@
 
 namespace skyss0fly\E-GapCooldown;
 
-use pocketmine\{event\Listener, plugin\PluginBase, item\GoldenAppleEnchanted, event\player\PlayerItemUseEvent, event\player\PlayerItemConsumeEvent};
+use pocketmine\{event\Listener, plugin\PluginBase, item\GoldenAppleEnchanted, event\player\PlayerItemUseEvent, event\player\PlayerItemConsumeEvent, inventory\PlayerInventory};
 
 class Main extends PluginBase implements Listener {
-
+private $cooldowns = [];
+  
 public function onLoad():void {
 $this->saveDefaultConfig();
   
 }
 public function onConsume(PlayerItemConsumeEvent $consumer, GoldenAppleEnchanted $egap) {
-$
+ $cooldown = $this->getConfig()->get("Cooldown");
+   $currentTime = time();
+$item = $player->getInventory()->getItemInHand();
+  if ($item === $egap){
+    $this->cooldowns[$player->getName()] = $currentTime + $cooldown;
+                if (isset($this->cooldowns[$player->getName()]) && $this->cooldowns[$player->getName()] > $currentTime) {
+                    $remainingTime = $this->cooldowns[$player->getName()] - $currentTime;
+                    $sender->sendMessage("§cError: still in timeout. Remaining time: " . $remainingTime . " seconds.");
+                    return false;
+                }
+  }
+  else {
+
+    // noop
+
+  }
+
+}
 }
 
 
 
 
 
-}
+
